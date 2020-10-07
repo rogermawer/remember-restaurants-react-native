c5cfb3b7943f2a08db5e7d0a4d2b42f2248077e9
RogerM <rogermawer@gmail.com>
2020-10-06 21:28:00 -0700

    moved location getting function to app.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
+import * as Location from "expo-location";
 
 import FindRestaurant from "./views/FindRestaurant";
 import Remembered from "./views/Remembered";
@@ -10,6 +11,28 @@ import Header from "./components/Header";
 export default function App() {
   const [searchResults, passSearchResults] = useState([]);
   const [allSavedRestaurants, setAllSavedRestaurants] = useState([]);
+  const [userLocation, setUserLocation] = useState(null);
+  const [locationError, setLocationError] = useState(null);
+
+  const getLocation = () => {
+    if (!userLocation) {
+      manuallyGetLocation();
+    }
+  };
+  getLocation();
+
+  async function manuallyGetLocation() {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setLocationError("Permission to access location was denied");
+    }
+    try {
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation(location);
+    } catch (e) {
+      setLocationError("Couldn't find your location. Is data on?");
+    }
+  }
 
   const updateSavedRestaurants = (restaurant) => {
     setAllSavedRestaurants([...allSavedRestaurants, restaurant]);
@@ -35,6 +58,9 @@ export default function App() {
               searchResults={searchResults}
               storeSearchResults={passSearchResults}
               addToSavedRestaurants={updateSavedRestaurants}
+              manuallyGetLocation={manuallyGetLocation}
+              userLocation={userLocation}
+              locationError={locationError}
             />
           )}
         </Tab.Screen>
